50fd6cd80219909ecb6a5725e337a01aaa9380e6
Matthew Champion <mattjameschampion@gmail.com>
2017-06-21 23:29:34 +0100

    Add AbstractMusicNote

---
+++ FretEngine/Common/DataTypes/AbstractMusicNote.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FretEngine.Common.DataTypes
+{
+    public enum AbstractMusicNote
+    {
+        CNatural,
+        CSharpDFlat,
+        DNatural,
+        DSharpEFlat,
+        ENatural,
+        FNatural,
+        FSharpGFlat,
+        GNatural,
+        GSharpAFlat,
+        ANatural,
+        ASharpBFlat,
+        BNatural
+    }
+}
