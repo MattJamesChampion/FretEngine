46b8bdfc897c014ada2452e4e32bb31eb3c6864a
Matthew Champion <mattjameschampion@gmail.com>
2017-06-25 15:38:51 +0100

    Add the basic implementation of the MusicNote class and related tests

---
+++ FretEngine/MusicLogic/MusicNote.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FretEngine.Common.DataTypes;
+
+namespace FretEngine.MusicLogic
+{
+    public class MusicNote
+    {
+        public AbstractMusicNote Value;
+        public int Octave;
+
+        public MusicNote(AbstractMusicNote value, int octave = 4)
+        {
+            Value = value;
+            Octave = octave;
+        }
+    }
+}
---
+++ UnitTests_FretEngine/MusicLogic/MusicNoteTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using FretEngine.Common.DataTypes;
+using FretEngine.MusicLogic;
+
+namespace UnitTests_FretEngine.MusicLogic
+{
+    [TestFixture]
+    class MusicNoteTests
+    {
+        [TestCase(AbstractMusicNote.BSharpCNatural, 0)]
+        [TestCase(AbstractMusicNote.DNatural, 2)]
+        [TestCase(AbstractMusicNote.ENaturalFFlat, 4)]
+        [TestCase(AbstractMusicNote.FSharpGFlat, 6)]
+        [TestCase(AbstractMusicNote.GSharpAFlat, 8)]
+        [TestCase(AbstractMusicNote.ASharpBFlat, 10)]
+        public void MusicNote_WhenConstructingWithValidAbstractMusicNoteAndValidOctave_ShouldNotThrowException(AbstractMusicNote value, int octave)
+        {
+            Assert.DoesNotThrow(() => {
+                new MusicNote(value, octave);
+            });
+        }
+
+        [TestCase(AbstractMusicNote.CSharpDFlat)]
+	    [TestCase(AbstractMusicNote.DSharpEFlat)]
+	    [TestCase(AbstractMusicNote.ESharpFNatural)]
+	    [TestCase(AbstractMusicNote.GNatural)]
+	    [TestCase(AbstractMusicNote.ANatural)]
+	    [TestCase(AbstractMusicNote.BNaturalCFlat)]
+        public void MusicNote_WhenConstructingWithValidAbstractMusicNoteAndNoOctave_ShouldNotThrowException(AbstractMusicNote value)
+        {
+            Assert.DoesNotThrow(() => {
+                new MusicNote(value);
+            });
+        }
+    }
+}
