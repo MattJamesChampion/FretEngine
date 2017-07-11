77931d34f4b22aa450787d96b18114ed6566b598
Matthew Champion <mattjameschampion@gmail.com>
2017-07-11 22:54:16 +0100

    Add MusicString and add related tests

---
+++ FretEngine/MusicLogic/MusicString.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FretEngine.Common.DataTypes;
+
+namespace FretEngine.MusicLogic
+{
+    public class MusicString
+    {
+        public MusicNote RootNote;
+
+        public MusicString(MusicNote rootNote)
+        {
+            RootNote = rootNote;
+        }
+    }
+}
---
+++ UnitTests_FretEngine/MusicLogic/MusicStringTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using FretEngine.Common.DataTypes;
+using FretEngine.MusicLogic;
+
+namespace UnitTests_FretEngine.MusicLogic
+{
+    [TestFixture]
+    class MusicStringTests
+    {
+        [TestCase(AbstractMusicNote.BSharpCNatural, 4)]
+        [TestCase(AbstractMusicNote.ASharpBFlat, 7)]
+        [TestCase(AbstractMusicNote.ESharpFNatural, 30)]
+        [TestCase(AbstractMusicNote.GSharpAFlat, -9)]
+        [TestCase(AbstractMusicNote.FSharpGFlat, 0)]
+        public void MusicString_WhenConstructingWithValidMusicNote_ShouldNotThrowException(AbstractMusicNote testAbstractMusicNote, int testOctave)
+        {
+            var testMusicNote = new MusicNote(testAbstractMusicNote, testOctave);
+
+            Assert.DoesNotThrow(() =>
+            {
+                new MusicString(testMusicNote);
+            });
+        }
+
+        [TestCase(AbstractMusicNote.BSharpCNatural, 4)]
+        [TestCase(AbstractMusicNote.ASharpBFlat, 17)]
+        [TestCase(AbstractMusicNote.ESharpFNatural, 2)]
+        [TestCase(AbstractMusicNote.FSharpGFlat, 0)]
+        [TestCase(AbstractMusicNote.CSharpDFlat, -8)]
+        public void MusicString_WhenConstructingWithValidMusicNote_ShouldContainCorrectValues(AbstractMusicNote testAbstractMusicNote, int testOctave)
+        {
+            var testMusicNote = new MusicNote(testAbstractMusicNote, testOctave);
+
+            var testMusicString = new MusicString(testMusicNote);
+
+            Assert.AreEqual(testMusicNote, testMusicString.RootNote);
+        }
+    }
+}
