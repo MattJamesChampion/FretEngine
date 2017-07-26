dbc380af87e7b3044071a4f1bff93f2334e63e53
Matthew Champion <mattjameschampion@gmail.com>
2017-07-26 01:08:05 +0100

    Override Equals, GetHashCode and the equality/inequality operators in MusicString and add related tests

--- FretEngine/MusicLogic/MusicString.cs
+++ FretEngine/MusicLogic/MusicString.cs
@@ -47,5 +47,121 @@ namespace FretEngine.MusicLogic
         {
             return string.Format("MusicString with root note: {0}", RootNote);
         }
+
+        /// <summary>
+        /// Determines whether a specified <see cref="object"/> has the same
+        /// value as the current instance.
+        /// </summary>
+        /// <param name="obj">
+        /// The <see cref="object"/> to compare with the current instance, or
+        /// null.
+        /// </param>
+        /// <returns>
+        /// true if the value of <paramref name="obj"/> is the same as the
+        /// current instance; otherwise, false. If <paramref name="obj"/> is
+        /// null, or cannot be cast as a valid <see cref="MusicString"/>, the
+        /// method returns false.
+        /// </returns>
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+
+            MusicString targetMusicString = obj as MusicString;
+
+            if ((object)targetMusicString == null)
+            {
+                return false;
+            }
+
+            return Equals(targetMusicString);
+        }
+
+        /// <summary>
+        /// Determines whether a specified <see cref="MusicString"/> object has
+        /// the same value as the current instance.
+        /// </summary>
+        /// <param name="targetMusicString">
+        /// The <see cref="MusicString"/> to compare with the current instance,
+        /// or null.
+        /// </param>
+        /// <returns>
+        /// true if the value of <paramref name="targetMusicString"/> is the
+		/// same as the current instance; otherwise, false. If
+		/// <paramref name="targetMusicString"/> is null, the method returns
+		/// false.
+        /// </returns>
+        public bool Equals(MusicString targetMusicString)
+        {
+            if ((object)targetMusicString == null)
+            {
+                return false;
+            }
+
+            return (RootNote.Equals(targetMusicString.RootNote));
+        }
+
+        /// <summary>
+        /// Returns the hash code for this <see cref="MusicString"/>.
+        /// </summary>
+        /// <returns>
+        /// The hash code for the current <see cref="MusicString"/>.
+        /// </returns>
+        public override int GetHashCode()
+        {
+            return RootNote.GetHashCode();
+        }
+
+        /// <summary>
+        /// Determines whether two specified instances of
+        /// <see cref="MusicString"/> have the same value.
+        /// </summary>
+        /// <param name="firstMusicString">
+        /// The first <see cref="MusicString"/> to compare, or null.
+        /// </param>
+        /// <param name="secondMusicString">
+        /// The second <see cref="MusicString"/> to compare, or null.
+        /// </param>
+        /// <returns>
+        /// true if the value of <paramref name="firstMusicString"/> is the
+		/// same as the value of <paramref name="secondMusicString"/>;
+		/// otherwise, false.
+        /// </returns>
+        public static bool operator == (MusicString firstMusicString, MusicString secondMusicString)
+        {
+            if (ReferenceEquals(firstMusicString, secondMusicString))
+            {
+                return true;
+            }
+
+            if (((object)firstMusicString == null) || ((object)secondMusicString == null))
+            {
+                return false;
+            }
+
+            return firstMusicString.Equals(secondMusicString);
+        }
+
+        /// <summary>
+        /// Determines whether two specified instances of
+        /// <see cref="MusicString"/> do not have the same value.
+        /// </summary>
+        /// <param name="firstMusicString">
+        /// The first <see cref="MusicString"/> to compare, or null.
+        /// </param>
+        /// <param name="secondMusicString">
+        /// The second <see cref="MusicString"/> to compare, or null.
+        /// </param>
+        /// <returns>
+        /// true if the value of <paramref name="firstMusicString"/> is not the
+        /// same as the value of <paramref name="secondMusicString"/>;
+		/// otherwise, false.
+        /// </returns>
+        public static bool operator != (MusicString firstMusicString, MusicString secondMusicString)
+        {
+            return !(firstMusicString == secondMusicString);
+        }
     }
 }
--- UnitTests_FretEngine/MusicLogic/MusicStringTests.cs
+++ UnitTests_FretEngine/MusicLogic/MusicStringTests.cs
@@ -57,5 +57,172 @@ namespace UnitTests_FretEngine.MusicLogic
                 testMusicString.ToString();
             });
         }
+        
+        [TestCase(AbstractMusicNote.GNatural, 2)]
+        [TestCase(AbstractMusicNote.BSharpCNatural, 3)]
+        [TestCase(AbstractMusicNote.DSharpEFlat, -6)]
+        [TestCase(AbstractMusicNote.BNaturalCFlat, 14)]
+        [TestCase(AbstractMusicNote.ANatural, 0)]
+        public void Equals_WhenComparingWithNull_ShouldReturnFalse(AbstractMusicNote testAbstractMusicNote, int testOctave)
+        {
+            var testMusicNote = new MusicNote(testAbstractMusicNote, testOctave);
+            var testMusicString = new MusicString(testMusicNote);
+
+            Assert.IsFalse(testMusicString.Equals(null));
+        }
+
+        [TestCase(AbstractMusicNote.DNatural, 19)]
+        [TestCase(AbstractMusicNote.GSharpAFlat, -28)]
+        [TestCase(AbstractMusicNote.CSharpDFlat, 4)]
+        [TestCase(AbstractMusicNote.BNaturalCFlat, 0)]
+        [TestCase(AbstractMusicNote.ENaturalFFlat, 7)]
+        public void Equals_WhenComparingEqualMusicStrings_ShouldReturnTrue(AbstractMusicNote testAbstractMusicNote, int testOctave)
+        {
+            var firstMusicNote = new MusicNote(testAbstractMusicNote, testOctave);
+            var firstMusicString = new MusicString(firstMusicNote);
+
+            var secondMusicNote = new MusicNote(testAbstractMusicNote, testOctave);
+            var secondMusicString = new MusicString(secondMusicNote);
+
+            Assert.IsTrue(firstMusicString.Equals(secondMusicString));
+        }
+
+        [TestCase(AbstractMusicNote.GSharpAFlat, 3, AbstractMusicNote.GSharpAFlat, -3)]
+        [TestCase(AbstractMusicNote.GNatural, 5, AbstractMusicNote.FSharpGFlat, 5)]
+        [TestCase(AbstractMusicNote.ANatural, 7, AbstractMusicNote.GSharpAFlat, 7)]
+        [TestCase(AbstractMusicNote.ENaturalFFlat, 2, AbstractMusicNote.ANatural, 19)]
+        [TestCase(AbstractMusicNote.ESharpFNatural, 5, AbstractMusicNote.ESharpFNatural, 6)]
+        public void Equals_WhenComparingUnequalMusicStrings_ShouldReturnFalse(AbstractMusicNote firstAbstractMusicNote, int firstOctave, AbstractMusicNote secondAbstractMusicNote, int secondOctave)
+        {
+            var firstMusicNote = new MusicNote(firstAbstractMusicNote, firstOctave);
+            var firstMusicString = new MusicString(firstMusicNote);
+
+            var secondMusicNote = new MusicNote(secondAbstractMusicNote, secondOctave);
+            var secondMusicString = new MusicString(secondMusicNote);
+
+            Assert.IsFalse(firstMusicString.Equals(secondMusicString));
+        }
+
+        [TestCase(AbstractMusicNote.GNatural, 2)]
+        [TestCase(AbstractMusicNote.ENaturalFFlat, 7)]
+        [TestCase(AbstractMusicNote.ASharpBFlat, 0)]
+        [TestCase(AbstractMusicNote.GSharpAFlat, -9)]
+        [TestCase(AbstractMusicNote.BNaturalCFlat, 14)]
+        public void GetHashCode_WhenGeneratedForEqualMusicStrings_ShouldBeEqual(AbstractMusicNote testAbstractMusicNote, int testOctave)
+        {
+            var firstMusicNote = new MusicNote(testAbstractMusicNote, testOctave);
+            var firstMusicString = new MusicString(firstMusicNote);
+
+            var secondMusicNote = new MusicNote(testAbstractMusicNote, testOctave);
+            var secondMusicString = new MusicString(secondMusicNote);
+
+            Assert.AreEqual(firstMusicString.GetHashCode(), secondMusicString.GetHashCode());
+        }
+
+        [TestCase(AbstractMusicNote.BNaturalCFlat, 3, AbstractMusicNote.BNaturalCFlat, 4)]
+        [TestCase(AbstractMusicNote.ENaturalFFlat, 0, AbstractMusicNote.ENaturalFFlat, 1)]
+        [TestCase(AbstractMusicNote.GNatural, 2, AbstractMusicNote.BNaturalCFlat, 9)]
+        [TestCase(AbstractMusicNote.GSharpAFlat, 7, AbstractMusicNote.GSharpAFlat, 8)]
+        [TestCase(AbstractMusicNote.ANatural, -4, AbstractMusicNote.ANatural, 4)]
+        public void GetHashCode_WhenGeneratedForUnequalMusicStrings_ShouldNotBeEqual(AbstractMusicNote firstAbstractMusicNote, int firstOctave, AbstractMusicNote secondAbstractMusicNote, int secondOctave)
+        {
+            var firstMusicNote = new MusicNote(firstAbstractMusicNote, firstOctave);
+            var firstMusicString = new MusicString(firstMusicNote);
+
+            var secondMusicNote = new MusicNote(secondAbstractMusicNote, secondOctave);
+            var secondMusicString = new MusicString(secondMusicNote);
+
+            Assert.AreNotEqual(firstMusicString.GetHashCode(), secondMusicString.GetHashCode());
+        }
+
+        [TestCase(AbstractMusicNote.CSharpDFlat, -4)]
+        [TestCase(AbstractMusicNote.BNaturalCFlat, 2)]
+        [TestCase(AbstractMusicNote.DSharpEFlat, -0)]
+        [TestCase(AbstractMusicNote.FSharpGFlat, 4)]
+        [TestCase(AbstractMusicNote.GSharpAFlat, 17)]
+        public void EqualityOperator_WhenComparingWithNull_ShouldReturnFalse(AbstractMusicNote testAbstractMusicNote, int testOctave)
+        {
+            var testMusicNote = new MusicNote(testAbstractMusicNote, testOctave);
+            var testMusicString = new MusicString(testMusicNote);
+
+            Assert.IsFalse(testMusicString == null);
+        }
+
+        [TestCase(AbstractMusicNote.FSharpGFlat, -4)]
+        [TestCase(AbstractMusicNote.DSharpEFlat, 3)]
+        [TestCase(AbstractMusicNote.BSharpCNatural, 0)]
+        [TestCase(AbstractMusicNote.ANatural, 6)]
+        [TestCase(AbstractMusicNote.DNatural, 15)]
+        public void EqualityOperator_WhenComparingEqualMusicStrings_ShouldReturnTrue(AbstractMusicNote testAbstractMusicNote, int testOctave)
+        {
+            var firstMusicNote = new MusicNote(testAbstractMusicNote, testOctave);
+            var firstMusicString = new MusicString(firstMusicNote);
+
+            var secondMusicNote = new MusicNote(testAbstractMusicNote, testOctave);
+            var secondMusicString = new MusicString(secondMusicNote);
+
+            Assert.IsTrue(firstMusicString == secondMusicString);
+        }
+
+        [TestCase(AbstractMusicNote.BSharpCNatural, -4, AbstractMusicNote.BSharpCNatural, 4)]
+        [TestCase(AbstractMusicNote.DNatural, 3, AbstractMusicNote.DNatural, 5)]
+        [TestCase(AbstractMusicNote.ASharpBFlat, 0, AbstractMusicNote.GSharpAFlat, 0)]
+        [TestCase(AbstractMusicNote.GSharpAFlat, -2, AbstractMusicNote.GSharpAFlat, -7)]
+        [TestCase(AbstractMusicNote.FSharpGFlat, 5, AbstractMusicNote.BSharpCNatural, 3)]
+        public void EqualityOperator_WhenComparingUnequalMusicStrings_ShouldReturnFalse(AbstractMusicNote firstAbstractMusicNote, int firstOctave, AbstractMusicNote secondAbstractMusicNote, int secondOctave)
+        {
+            var firstMusicNote = new MusicNote(firstAbstractMusicNote, firstOctave);
+            var firstMusicString = new MusicString(firstMusicNote);
+
+            var secondMusicNote = new MusicNote(secondAbstractMusicNote, secondOctave);
+            var secondMusicString = new MusicString(secondMusicNote);
+
+            Assert.IsFalse(firstMusicString == secondMusicString);
+        }
+
+        [TestCase(AbstractMusicNote.ESharpFNatural, 1)]
+        [TestCase(AbstractMusicNote.ENaturalFFlat, 9)]
+        [TestCase(AbstractMusicNote.ASharpBFlat, 0)]
+        [TestCase(AbstractMusicNote.BSharpCNatural, 14)]
+        [TestCase(AbstractMusicNote.GNatural, -12)]
+        public void InequalityOperator_WhenComparingWithNull_ShouldReturnTrue(AbstractMusicNote testAbstractMusicNote, int testOctave)
+        {
+            var testMusicNote = new MusicNote(testAbstractMusicNote, testOctave);
+            var testMusicString = new MusicString(testMusicNote);
+
+            Assert.IsTrue(testMusicString != null);
+        }
+
+        [TestCase(AbstractMusicNote.GNatural, 7)]
+        [TestCase(AbstractMusicNote.ESharpFNatural, 5)]
+        [TestCase(AbstractMusicNote.GSharpAFlat, -24)]
+        [TestCase(AbstractMusicNote.ANatural, 0)]
+        [TestCase(AbstractMusicNote.CSharpDFlat, 19)]
+        public void InequalityOperator_WhenComparingEqualMusicStrings_ShouldReturnFalse(AbstractMusicNote testAbstractMusicNote, int testOctave)
+        {
+            var firstMusicNote = new MusicNote(testAbstractMusicNote, testOctave);
+            var firstMusicString = new MusicString(firstMusicNote);
+
+            var secondMusicNote = new MusicNote(testAbstractMusicNote, testOctave);
+            var secondMusicString = new MusicString(secondMusicNote);
+
+            Assert.IsFalse(firstMusicString != secondMusicString);
+        }
+
+        [TestCase(AbstractMusicNote.ENaturalFFlat, 12, AbstractMusicNote.BSharpCNatural, 3)]
+        [TestCase(AbstractMusicNote.CSharpDFlat, 4, AbstractMusicNote.DSharpEFlat, -8)]
+        [TestCase(AbstractMusicNote.BNaturalCFlat, 6, AbstractMusicNote.BNaturalCFlat, 16)]
+        [TestCase(AbstractMusicNote.ASharpBFlat, 3, AbstractMusicNote.BNaturalCFlat, 3)]
+        [TestCase(AbstractMusicNote.DNatural, -17, AbstractMusicNote.DNatural, 17)]
+        public void InequalityOperator_WhenComparingUnequalMusicStrings_ShouldReturnTrue(AbstractMusicNote firstAbstractMusicNote, int firstOctave, AbstractMusicNote secondAbstractMusicNote, int secondOctave)
+        {
+            var firstMusicNote = new MusicNote(firstAbstractMusicNote, firstOctave);
+            var firstMusicString = new MusicString(firstMusicNote);
+
+            var secondMusicNote = new MusicNote(secondAbstractMusicNote, secondOctave);
+            var secondMusicString = new MusicString(secondMusicNote);
+            
+            Assert.IsTrue(firstMusicString != secondMusicString);
+        }
     }
 }
